1ff425a5ee68fe32737aaaa97d0696dfad189375
virgilshi <2338052892@qq.com>
2020-09-27 15:36:40 +0800

    Create calcG-M-K.cc

---
+++ calcG-M-K.cc
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <bits/stdc++.h>
+using namespace std;
+#define K 1024ULL
+#define M (K * K) 
+#define G (M * K)
+
+int main() {
+	printf("%lf\n", 4.8 * G);
+	cout << sizeof(unsigned long long) << endl;
+	double a = 5153960755;
+	size_t b = 5153960755;
+	printf("%lf\n", a);
+	cout << b << endl;
+}
